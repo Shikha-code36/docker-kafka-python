481ceb590ddd8926b17d6e6222fe01b3662c0a74
Shikha <shikha.py36@gmail.com>
2023-03-02 11:31:07 +0530

    kafka-producer

--- backend/producer/producer.py
+++ backend/producer/producer.py
@@ -1,25 +1,27 @@
 from flask import Blueprint, jsonify, request
 from kafka import KafkaProducer
+from PIL import Image
+import io
 import base64
 import json
-import cv2
-import numpy as np
 
 producer_bp = Blueprint('producer_bp', __name__)
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+)
 
 @producer_bp.route('/api/frame', methods=['POST'])
 def send_frame():
     # get the frame from the request
-    frame_data = request.data
-    nparr = np.fromstring(frame_data, np.uint8)
-    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    frame_data = request.json['imageData']
+    image = Image.open(io.BytesIO(base64.b64decode(frame_data.split(',')[1])))
 
     # encode the frame as a base64 string
-    retval, buffer = cv2.imencode('.jpg', frame)
-    data = base64.b64encode(buffer).decode('utf-8')
+    buffer = io.BytesIO()
+    image.save(buffer, format='JPEG')
+    data = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
     # create a dictionary to store the frame data
     frame_dict = {'data': data}
